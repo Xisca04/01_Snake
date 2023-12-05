51aa0a59289c936957b5c0cbc1eeecfaa00a485c
Xisca04 <114388909+Xisca04@users.noreply.github.com>
2023-12-05 13:30:17 +0100

    loadScene_slider

--- Assets/Scripts/LoaderCallback.cs
+++ Assets/Scripts/LoaderCallback.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class LoaderCallback : MonoBehaviour
 {
     private bool firstUpdate = true;
+    private float sliderTimeLeft = 1.85f;
 
     private void Update()  // Poner contador o corrutina si queremos que dure más la escena de carga
     {
         if (firstUpdate)
         {
             firstUpdate = false;
-            Loader.LoaderCallBack();
+            StartCoroutine("LoadChargerSlider");
         }
+    }
+
+    // corruitina más tiempo visual escena de carga
+
+    private IEnumerator LoadChargerSlider()
+    {
+        yield return new WaitForSeconds(sliderTimeLeft);
+        Loader.LoaderCallBack();
     }
 }
